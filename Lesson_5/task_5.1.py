0c7ecdc80ca24aeea8bf130ca46833531432452f
kirin <Matvey.zobachev@gmail.com>
2021-02-08 00:54:21 +0300

    upload l4

---
+++ Lesson_5/task_5.1.py
@@ -0,0 +1,21 @@
+# Создать программно файл в текстовом формате, записать в него построчно данные,
+# вводимые пользователем. Об окончании ввода данных свидетельствует пустая строка.
+
+with open('my_fil.txt', 'w') as my_file:
+    my_text = input('enter text\n')
+    while my_text:
+        my_file.writelines(my_text)
+        my_text = input('enter text\n')
+        if not my_text:
+            break
+
+
+
+
+
+with open('my_fil.txt', 'r') as my_file:
+    while True:
+        content = my_file.read()
+        print(content)
+        if not content:
+            break
